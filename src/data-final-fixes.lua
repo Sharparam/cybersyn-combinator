7eed25d4feaf8c7345d616e7d4e08c44f1f1446e
Adam Hellberg <sharparam@sharparam.com>
2023-03-23 02:21:14 +0100

    Add setting for upgradeability

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -6,3 +6,9 @@ if mods["cybersyn"] and data.raw.technology["cybersyn-train-network"] then
 else
   table.insert(data.raw.technology["circuit-network"].effects, { type = "unlock-recipe", recipe = constants.ENTITY_NAME })
 end
+
+if settings.startup["cybersyn-combinator-upgradeable"].value == true then
+  data.raw["constant-combinator"]["constant-combinator"].next_upgrade = constants.ENTITY_NAME
+end
+
+data.raw["constant-combinator"]["constant-combinator"].fast_replaceable_group = "constant-combinator"
---
+++ src/settings.lua
@@ -0,0 +1,9 @@
+data:extend {
+  {
+    type = "bool-setting",
+    name = "cybersyn-combinator-upgradeable",
+    setting_type = "startup",
+    default_value = false,
+    order = "c[cybersyn]-c[combinator]-u[upgradeable]"
+  }
+}
