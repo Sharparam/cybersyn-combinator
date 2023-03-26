69f8f1c963fa7e077941cb417f7a1dee0ff4ca47
Adam Hellberg <sharparam@sharparam.com>
2023-03-26 05:45:32 +0200

    Add more settings

--- src/scripts/constants.lua
+++ src/scripts/constants.lua
@@ -3,6 +3,20 @@ local constants = {
   INT32_MIN = -2147483648,
   MOD_NAME = "cybersyn-combinator",
   MOD_TITLE = "Cybersyn Combinator",
+  SETTINGS = {
+    LOG_LEVEL = "cybersyn-combinator-loglevel",
+    CHAT_LOG_LEVEL = "cybersyn-combinator-loglevel-chat",
+    SLOT_COUNT = "cybersyn-combinator-slotcount",
+    SLOT_ROWS = "cybersyn-combinator-slotrows",
+    SLOT_COUNT_WAGON = "cybersyn-combinator-slotcount-wagon",
+    EMIT_DEFAULT_REQUEST_THRESHOLD = "cybersyn-combinator-emit-default-request-threshold",
+    EMIT_DEFAULT_PRIORITY = "cybersyn-combinator-emit-default-priority",
+    EMIT_DEFAULT_LOCKED_SLOTS = "cybersyn-combinator-emit-default-locked-slots",
+    USE_STACKS = "cybersyn-combinator-use-stacks",
+    CS_REQUEST_THRESHOLD = "cybersyn-request-threshold",
+    CS_PRIORITY = "cybersyn-priority",
+    CS_LOCKED_SLOTS = "cybersyn-locked-slots"
+  },
   ENTITY_NAME = "cybersyn-constant-combinator"
 }
 
--- src/settings.lua
+++ src/settings.lua
@@ -1,9 +1,82 @@
 data:extend {
   {
+    type = "string-setting",
+    name = "cybersyn-combinator-loglevel",
+    setting_type = "startup",
+    default_value = "WARN",
+    allowed_values = {
+      "DEBUG",
+      "INFO",
+      "WARN",
+      "ERROR",
+      "DISABLED"
+    },
+    order = "c[cybersyn]-c[combinator]-l[loglevel]"
+  },
+  {
+    type = "string-setting",
+    name = "cybersyn-combinator-loglevel-chat",
+    setting_type = "startup",
+    default_value = "ERROR",
+    allowed_values = {
+      "DEBUG",
+      "INFO",
+      "WARN",
+      "ERROR",
+      "DISABLED"
+    },
+    order = "c[cybersyn]-c[combinator]-l[loglevel]-c[chat]"
+  },
+  {
+    type = "int-setting",
+    name = "cybersyn-combinator-slotrows",
+    setting_type = "startup",
+    default_value = 4,
+    minimum_value = 1,
+    maximum_value = 64,
+    order = "c[cybersyn]-c[combinator]-s[slotrows]"
+  },
+  {
+    type = "int-setting",
+    name = "cybersyn-combinator-slotcount-wagon",
+    setting_type = "startup",
+    default_value = 1000,
+    minimum_value = 1,
+    order = "c[cybersyn]-c[combinator]-s[slotcount]-w[wagon]"
+  },
+  {
     type = "bool-setting",
     name = "cybersyn-combinator-upgradeable",
     setting_type = "startup",
     default_value = false,
     order = "c[cybersyn]-c[combinator]-u[upgradeable]"
+  },
+  {
+    type = "bool-setting",
+    name = "cybersyn-combinator-emit-default-request-threshold",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "c[cybersyn]-c[combinator]-e[emit]-d[default]-r[request]-t[threshold]"
+  },
+  {
+    type = "bool-setting",
+    name = "cybersyn-combinator-emit-default-priority",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "c[cybersyn]-c[combinator]-e[emit]-d[default]-p[priority]"
+  },
+  {
+    type = "bool-setting",
+    name = "cybersyn-combinator-emit-default-locked-slots",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "c[cybersyn]-c[combinator]-e[emit]-d[default]-l[locked]-s[slots]"
+  },
+  {
+    type = "bool-setting",
+    name = "cybersyn-combinator-use-stacks",
+    setting_type = "runtime-per-user",
+    default_value = false,
+    order = "c[cybersyn]-c[combinator]-u[use]-s[stacks]"
   }
 }
