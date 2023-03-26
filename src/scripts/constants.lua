b69c4c2583f77fa2a57a23c1e68bfd28c01773c7
Adam Hellberg <sharparam@sharparam.com>
2023-03-26 05:16:42 +0200

    Add constant for close sound

--- src/scripts/constants.lua
+++ src/scripts/constants.lua
@@ -3,4 +3,7 @@ local constants = {
   ENTITY_NAME = "cybersyn-constant-combinator"
 }
 
+--- @type string
+constants.ENTITY_CLOSE_SOUND = "entity-close/" .. constants.ENTITY_NAME
+
 return constants
