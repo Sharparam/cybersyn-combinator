5e6c6fb06b059bb034191dc158342124e5842c8a
Adam Hellberg <sharparam@sharparam.com>
2023-03-28 01:12:28 +0200

    Fix some formatting

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -1,8 +1,9 @@
 local constants = require "scripts.constants"
 
 if mods["cybersyn"] and data.raw.technology["cybersyn-train-network"] then
-  table.insert(data.raw.technology["cybersyn-train-network"].effects,
-  { type = "unlock-recipe", recipe = constants.ENTITY_NAME })
+  table.insert(
+    data.raw.technology["cybersyn-train-network"].effects,
+    { type = "unlock-recipe", recipe = constants.ENTITY_NAME })
 else
   table.insert(data.raw.technology["circuit-network"].effects, { type = "unlock-recipe", recipe = constants.ENTITY_NAME })
 end
