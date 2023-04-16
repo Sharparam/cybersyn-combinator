d85926a27570056b24976ff0433acc885ca24def
Adam Hellberg <sharparam@sharparam.com>
2023-04-16 19:18:22 +0200

    Fix issue with new expression evaluator
    `game.evaluate_expression` was not able to parse expressions starting
    with a negative sign ("-").

--- src/scripts/expression.lua
+++ src/scripts/expression.lua
@@ -2,6 +2,7 @@ local log = require("scripts.logger").expression
 local gsub = string.gsub
 
 local NONMATH_PATTERN = "[^0-9%.+%-*/%%^()]"
+local ssub = string.sub
 
 local expr = {}
 
@@ -12,6 +13,10 @@ function expr.parse(input, fallback)
   fallback = fallback or 0
   log:debug("parsing expression: ", input)
   if not input then return fallback end
+
+  if ssub(input, 1, 1) == "-" then
+    input = "0" .. input
+  end
 
   local success, result = pcall(game.evaluate_expression, input)
 
