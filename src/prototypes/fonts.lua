444a9feedd19f2cab98bf3b4c4411fd2b5b62481
Adam Hellberg <sharparam@sharparam.com>
2024-12-12 23:08:57 +0100

    Add missing font prototypes file

---
+++ src/prototypes/fonts.lua
@@ -0,0 +1,10 @@
+data:extend {
+  {
+    type = "font",
+    name = "cybersyn-combinator_signal-comparator-font",
+    from = "default-bold",
+    size = 13,
+    border = true,
+    border_color = {}
+  }
+}
