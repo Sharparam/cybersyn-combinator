02968d24b6f1d180631ca61040a2287e372064f5
Adam Hellberg <sharparam@sharparam.com>
2025-01-02 11:00:31 +0100

    Add support for suffixes in expression input

--- src/scripts/expression.lua
+++ src/scripts/expression.lua
@@ -3,6 +3,17 @@ local ssub = string.sub
 
 local expr = {}
 
+local expr_vars = {
+  k = 1000,
+  K = 1000,
+  m = 1000000,
+  M = 1000000,
+  g = 1000000000,
+  G = 1000000000,
+  b = 1000000000,
+  B = 1000000000
+}
+
 --- @param input string?
 --- @param fallback number?
 --- @return number
@@ -15,7 +26,7 @@ function expr.parse(input, fallback)
     input = "0" .. input
   end
 
-  local success, result = pcall(helpers.evaluate_expression, input)
+  local success, result = pcall(helpers.evaluate_expression, input, expr_vars)
 
   if not success then
     log:warn("The given input '", input, "' could not be evaluated as a math expression: ", result)
